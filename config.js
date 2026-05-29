b64af1b9d51da76928fd991a8aed376bb28b6df7
erikkotkas <erik@designedbyerik.com>
2026-05-29 17:51:10 +0100

    turn off demo mode

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DEMO_MODE = true;
-const DEMO_SKIP_BUTTON = true;
+const DEMO_MODE = false;
+const DEMO_SKIP_BUTTON = false;
 const DEMO_UNDER_CONSTRUCTION = [1, 2];
-const DEMO_INACTIVITY = true;
+const DEMO_INACTIVITY = false;
